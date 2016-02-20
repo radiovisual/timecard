220d665e6f89c888451e0ba025d0cf6ab53a5831
Michael Wuergler <wuergler@gmail.com>
2016-02-20 20:53:15 +0000

    linting via xo

--- tc_messages.js
+++ tc_messages.js
@@ -1,47 +1,41 @@
 'use strict';
-
 var chalk = require('chalk');
 var convert = require('convert-seconds');
 var pendel = require('pendel');
 var zp = require('simple-zeropad');
 var to24 = require('twelve-to-twentyfour');
 
-
 /**
  * Error messages.
  *
  */
+
 module.exports.errors = {
-
-    'clockOutIsPending':    "\n" + chalk.bgRed("TIMECARD ERROR:") + chalk.bold.white(" You must clockout before clocking in. \n")+ chalk.bold.white("Tip:") +" Clock out with the "+chalk.cyan("clockout")+" command, or edit the timecard file manually. \n",
-    'noClockInFound':       "\n" + chalk.bgRed("TIMECARD ERROR:") + chalk.bold.white(" You must clockin before clocking out. \n")+ chalk.bold.white("Tip:") +" Clock in with the "+chalk.cyan("clockin")+" command, or edit the timecard file manually. \n"
+	clockOutIsPending: '\n' + chalk.bgRed('TIMECARD ERROR:') + chalk.bold.white(' You must clockout before clocking in. \n') + chalk.bold.white('Tip:') + ' Clock out with the ' + chalk.cyan('clockout') + ' command, or edit the timecard file manually. \n',
+	noClockInFound: '\n' + chalk.bgRed('TIMECARD ERROR:') + chalk.bold.white(' You must clockin before clocking out. \n') + chalk.bold.white('Tip:') + ' Clock in with the ' + chalk.cyan('clockin') + ' command, or edit the timecard file manually. \n'
 };
-
 
 /**
  * General messages.
  *
  */
 module.exports.messages = {
-
-    'createdNewTimeCard':   "\n  "+chalk.bgCyan.black("TIMECARD:") + chalk.bold.white(" You have created a new timecard file. \n")+ chalk.bold.white("  Tip:") +" Clock in with the "+chalk.cyan("clockin")+" command, Clock out with the "+chalk.cyan("clockout")+" command \n",
-    'successfulClockin':    "\n  "+chalk.bgCyan.black("TIMECARD:") + chalk.bold.white(" You have ") + chalk.bold.green("clocked in: ") + time() + "\n",
-    'successfulClockout':   "\n  "+chalk.bgCyan.black("TIMECARD:") + chalk.bold.white(" You have ") + chalk.bold.red("clocked out: ") + time() + "\n",
-    'prettyPrintHeader':    "\n  "+chalk.bgCyan.black("TIMECARD:") + chalk.cyan(" Logged hours") + "\n\n  "+chalk.gray("______________________________________________\n"),
-    'prettyPrintBorder':    chalk.gray("\n  ______________________________________________")
-
+	createdNewTimeCard: '\n  ' + chalk.bgCyan.black('TIMECARD:') + chalk.bold.white(' You have created a new timecard file. \n') + chalk.bold.white('  Tip:') + ' Clock in with the ' + chalk.cyan('clockin') + ' command, Clock out with the ' + chalk.cyan('clockout') + ' command \n',
+	successfulClockin: '\n  ' + chalk.bgCyan.black('TIMECARD:') + chalk.bold.white(' You have ') + chalk.bold.green('clocked in: ') + time() + '\n',
+	successfulClockout: '\n  ' + chalk.bgCyan.black('TIMECARD:') + chalk.bold.white(' You have ') + chalk.bold.red('clocked out: ') + time() + '\n',
+	prettyPrintHeader: '\n  ' + chalk.bgCyan.black('TIMECARD:') + chalk.cyan(' Logged hours') + '\n\n  ' + chalk.gray('______________________________________________\n'),
+	prettyPrintBorder: chalk.gray('\n  ______________________________________________')
 };
-
 
 /**
  * Output data to the console.
  *
  * @param {string} str
  */
-module.exports.print = function(str){
-    console.log("\n  "+chalk.gray(str));
-};
 
+module.exports.print = function (str) {
+	console.log('\n  ' + chalk.gray(str));
+};
 
 /**
  * A Helper function to generate the total time data.
@@ -51,65 +45,59 @@ module.exports.print = function(str){
  *
  * @param {number} seconds - the number of seconds recorded in the timecard project.
  */
-module.exports.summary = function(seconds){
 
-    var total = convert(seconds);
-    console.log( "\n  "+chalk.bgCyan.black("TIMECARD:") + chalk.cyan(" TOTAL TIME") + "\n" );
-    console.log( "  "+chalk.white("Hours: ")   + total.hours );
-    console.log( "  "+chalk.white("Minutes: ") + total.minutes );
-    console.log( "  "+chalk.white("Seconds: ") + total.seconds );
-    console.log();
-
+module.exports.summary = function (seconds) {
+	var total = convert(seconds);
+	console.log('\n  ' + chalk.bgCyan.black('TIMECARD:') + chalk.cyan(' TOTAL TIME') + '\n');
+	console.log('  ' + chalk.white('Hours: ') + total.hours);
+	console.log('  ' + chalk.white('Minutes: ') + total.minutes);
+	console.log('  ' + chalk.white('Seconds: ') + total.seconds);
+	console.log();
 };
-
 
 /**
  * Pretty-print the timecard data.
  *
  * @param {object} timeobj - the timecard data object
  */
-module.exports.prettyPrintEntry = function(timeobj){
 
-    var time = makeTimeString(pendel(timeobj.startTime, timeobj.endTime));
+module.exports.prettyPrintEntry = function (timeobj) {
+	var time = makeTimeString(pendel(timeobj.startTime, timeobj.endTime));
 
-    var tasks;
+	var tasks;
 
-    if (timeobj.hasOwnProperty("tasks") && timeobj.tasks.length > 0){
+	if (timeobj.hasOwnProperty('tasks') && timeobj.tasks.length > 0) {
+		tasks = '';
 
-        tasks = "";
+		for (var i = 0; i < timeobj.tasks.length; i++) {
+			tasks += '   - ' + timeobj.tasks[i] + '\n';
+		}
+	}
+	console.log('  ' + chalk.white(timeobj.date) + chalk.cyan(' ' + to24(timeobj.startTime) + ' - ' + to24(timeobj.endTime)) + chalk.gray(' [') + time + chalk.gray(']'));
 
-        for (var i = 0; i < timeobj.tasks.length; i++){
-            tasks += "   - "+timeobj.tasks[i]+"\n";
-        }
-    }
-
-    console.log( "  "+chalk.white(timeobj.date) + chalk.cyan(" "+to24(timeobj.startTime) +" - " + to24(timeobj.endTime)) + chalk.gray(" [")+time+chalk.gray("]") );
-
-    if (tasks) {
-        console.log("\n"+chalk.gray(tasks)+"\n");
-    }
-
+	if (tasks) {
+		console.log('\n' + chalk.gray(tasks) + '\n');
+	}
 };
 
-
 /**
- * Get the current time in the format: "Wed Apr 09 2015"
+ * Get the current time in the format: 'Wed Apr 09 2015'
  *
  * @returns {string}
  */
-function time(){
-    var date = new Date().toString();
-    return date.slice(16, 24)
+
+function time() {
+	var date = new Date().toString();
+	return date.slice(16, 24);
 }
 
-
 /**
- * Make a timestring in the format of "00:00:00"
+ * Make a timestring in the format of '00:00:00'
  *
  * @param {object} timeObj - the duration object returned by pendel.
  * @returns {string}
  */
-function makeTimeString(timeObj){
-    return zp(timeObj.hours)+":"+zp(timeObj.minutes)+":"+zp(timeObj.seconds);
 
+function makeTimeString(timeObj) {
+	return zp(timeObj.hours) + ':' + zp(timeObj.minutes) + ':' + zp(timeObj.seconds);
 }
--- timecard.js
+++ timecard.js
@@ -1,6 +1,5 @@
 #! /usr/bin/env node
 'use strict';
-
 var fs = require('fs');
 var objectAssign = require('object-assign');
 var parseArgs = require('minimist');
@@ -15,7 +14,7 @@ var eachAsync = require('each-async');
 var Promise = require('pinkie-promise');
 var duration = require('pendel');
 var multiline = require('multiline');
-var inquirer = require("inquirer");
+var inquirer = require('inquirer');
 
 var options = parseArgs(process.argv.slice(2));
 
@@ -28,6 +27,7 @@ delete options._;
  *
  * @note: This value is assigned in `init()`
  */
+
 var cliTimecard;
 
 /**
@@ -36,158 +36,128 @@ var cliTimecard;
  * @param {Object} options
  * @constructor
  */
-function TimeCard (options){
 
-    if (!(this instanceof TimeCard)){
-        return new TimeCard(options);
-    }
+function TimeCard(options) {
+	if (!(this instanceof TimeCard)) {
+		return new TimeCard(options);
+	}
 
-    this.options = objectAssign({}, options);
+	this.options = objectAssign({}, options);
 
-    this.filepath = this.options.filepath || process.cwd()+"/.timecard.json";
-    this.hours = [];
-    this.clockoutIsPending = false;
-    this.pendingClockoutIndex = null;
-    this.totalSeconds = 0;
-
+	this.filepath = this.options.filepath || process.cwd() + '/.timecard.json';
+	this.hours = [];
+	this.clockoutIsPending = false;
+	this.pendingClockoutIndex = null;
+	this.totalSeconds = 0;
 }
-
 
 /**
  * Clockin.
  *
  * @param {function} cb
  */
+
 TimeCard.prototype.clockin = function (cb) {
+	cb = cb || function () {};
 
-    cb = cb || function(){};
+	var self = this;
 
-    var self = this;
+	var TC = getTimeCardData(this.filepath).then(function (timeCardData) {
+		return self.processTimeCardData(timeCardData);
+	}).then(function (timeCardData) {
+		if (self.clockoutIsPending) {
+			cb(new Error(errors.clockOutIsPending));
+			return;
+		}
+		var date = new Date().toString();
 
-    var TC = getTimeCardData(this.filepath).then(function(timeCardData) {
+		var tc = {};
+		tc.date = date.slice(0, 15);
+		tc.startTime = date.slice(16, 24);
 
-        return self.processTimeCardData(timeCardData);
+		timeCardData.push(tc);
 
-    }).then(function (timeCardData) {
-
-        if (self.clockoutIsPending){
-            cb(new Error(errors.clockOutIsPending));
-            return;
-        }
-
-        var date = new Date().toString();
-
-        var tc = {};
-        tc.date = date.slice(0, 15);
-        tc.startTime = date.slice(16, 24);
-
-        timeCardData.push(tc);
-
-        self.writeTimeCard(JSON.stringify(timeCardData), reportSuccessfulClockIn);
-
-    });
-
-
-    TC.catch(function (err) {
-
-        if (err) {
-            cb(err);
-        }
-
-    });
-
-
+		self.writeTimeCard(JSON.stringify(timeCardData), reportSuccessfulClockIn);
+	});
+	TC.catch(function (err) {
+		if (err) {
+			cb(err);
+		}
+	});
 };
-
 
 /**
  * Clockout.
  *
  * @param {function} cb - the callback function
  */
+
 TimeCard.prototype.clockout = function (cb) {
+	cb = cb || function () {};
 
-    cb = cb || function(){};
+	var self = this;
 
-    var self = this;
+	var TC = getTimeCardData(this.filepath).then(function (timeCardData) {
+		return self.processTimeCardData(timeCardData);
+	}).then(function () {
+		if (self.clockoutIsPending) {
+			var date = new Date().toString();
 
-    var TC = getTimeCardData(this.filepath).then(function(timeCardData) {
-
-        return self.processTimeCardData(timeCardData);
-
-    }).then(function (timeCardData) {
-
-        if (self.clockoutIsPending){
-
-            var date = new Date().toString();
-
-            self.hours[self.pendingClockoutIndex].endTime = date.slice(16, 24);
-
-            self.writeTimeCard(JSON.stringify(self.hours), reportSuccessfulClockOut);
-
-
-        } else {
-            cb(new Error(errors.noClockInFound));
-        }
-
-    });
-
-    TC.catch(function (err) {
-
-        if (err) {
-            cb(err);
-        }
-
-    });
-
+			self.hours[self.pendingClockoutIndex].endTime = date.slice(16, 24);
+			self.writeTimeCard(JSON.stringify(self.hours), reportSuccessfulClockOut);
+		} else {
+			cb(new Error(errors.noClockInFound));
+		}
+	});
+	TC.catch(function (err) {
+		if (err) {
+			cb(err);
+		}
+	});
 };
 
 /**
  * Create a blank timecard on `timecard new`
  *
  */
-TimeCard.prototype.createBlankTimeCard = function(){
 
-    var self = this;
+TimeCard.prototype.createBlankTimeCard = function () {
+	var self = this;
 
-    // First check to see if the file already exists
-    fs.stat(self.filepath, function(err, stats){
+	// First check to see if the file already exists
+	fs.stat(self.filepath, function (err, stats) {
+		if (err && err.code === 'ENOENT') {
+			self.writeTimeCard('[]', reportSuccessfulNewTimeCard);
+		} else if (err) {
+			throw err;
+		}
+		if (stats && stats.isFile()) {
+			var question = [{
+				type: 'confirm',
+				name: 'eraseCard',
+				message: 'A timecard file already exists. Do you want to erase it and start over?',
+				default: false
+			}];
 
-        if (err && err.code === 'ENOENT'){
-            self.writeTimeCard('[]', reportSuccessfulNewTimeCard);
-        } else if (err) {
-            throw err;
-        }
-
-        if (stats && stats.isFile()){
-
-            var question = [{
-                    type: "confirm",
-                    name: "eraseCard",
-                    message: "A timecard file already exists. Do you want to erase it and start over?",
-                    default: false
-                }];
-
-            inquirer.prompt(question, function(answer) {
-                if (answer.eraseCard == true){
-                    self.writeTimeCard('[]', reportSuccessfulNewTimeCard);
-                } else {
-                    process.exit();
-                }
-            });
-        }
-    });
+			inquirer.prompt(question, function (answer) {
+				if (answer.eraseCard === true) {
+					self.writeTimeCard('[]', reportSuccessfulNewTimeCard);
+				} else {
+					process.exit();
+				}
+			});
+		}
+	});
 };
-
 
 /**
  * Get the hour array recorded by Timecard.
  *
  */
-TimeCard.prototype.hours = function(){
-    return this.hours;
-};
 
+TimeCard.prototype.hours = function () {
+	return this.hours;
+};
 
 /**
  * Record the times and look for conditions on the timecard data.
@@ -195,83 +165,71 @@ TimeCard.prototype.hours = function(){
  * @param timeCardData
  * @returns {*} Promise
  */
-TimeCard.prototype.processTimeCardData = function(timeCardData){
 
-    var self = this;
+TimeCard.prototype.processTimeCardData = function (timeCardData) {
+	var self = this;
 
-    return new Promise(function(resolve, reject){
+	return new Promise(function (resolve, reject) {
+		eachAsync(timeCardData, function (item, index, next) {
+			if (item.hasOwnProperty('startTime') && !item.hasOwnProperty('endTime')) {
+				self.clockoutIsPending = true;
+				self.pendingClockoutIndex = index;
+			}
 
-        eachAsync(timeCardData, function(item, index, next){
+			// get the number of minutes for all completed timecard sessions
+			if (item.hasOwnProperty('startTime') && item.hasOwnProperty('endTime')) {
+				self.totalSeconds += duration(item.startTime, item.endTime).totalSeconds;
+			}
 
-            if (item.hasOwnProperty("startTime") && !item.hasOwnProperty("endTime")){
-                self.clockoutIsPending = true;
-                self.pendingClockoutIndex = index;
-            }
+			self.hours.push(item);
 
-            // get the number of minutes for all completed timecard sessions
-            if(item.hasOwnProperty("startTime") && item.hasOwnProperty("endTime")){
-                self.totalSeconds += duration(item.startTime, item.endTime).totalSeconds;
-            }
-
-            self.hours.push(item);
-
-            next();
-
-        }, function(error){
-            if (error) { reject(error); }
-            resolve(timeCardData);
-        });
-
-    });
+			next();
+		}, function (error) {
+			if (error) {
+				reject(error);
+			}
+			resolve(timeCardData);
+		});
+	});
 };
-
 
 /**
  * Print the timecard data to the console.
  *
  * @param {function} cb - the callback
  */
-TimeCard.prototype.print = function(cb) {
 
-    cb = cb || function(){};
+TimeCard.prototype.print = function (cb) {
+	cb = cb || function () {};
 
-    var self = this;
+	var self = this;
 
-    var TC = getTimeCardData(this.filepath).then(function(timeCardData) {
+	var TC = getTimeCardData(this.filepath).then(function (timeCardData) {
+		return self.processTimeCardData(timeCardData);
+	}).then(function (timeCardData) {
+		console.log(messages.prettyPrintHeader);
 
-        return self.processTimeCardData(timeCardData);
+		eachAsync(timeCardData, function (item, index, next) {
+			prettyprint(item);
 
-    }).then(function (timeCardData) {
+			next();
+		}, function (error) {
+			if (error) {
+				throw error;
+			}
 
-        console.log(messages.prettyPrintHeader);
+			console.log(messages.prettyPrintBorder);
+			summary(self.totalSeconds);
 
-        eachAsync(timeCardData, function(item, index, next){
-
-            prettyprint(item);
-
-            next();
-
-        }, function(error){
-            if (error) { reject(error); }
-
-            console.log(messages.prettyPrintBorder);
-            summary(self.totalSeconds);
-
-            console.log();
-        });
-
-    });
-
-    TC.catch(function (err) {
-
-        if (err) {
-            cb(err);
-        }
-
-    });
-
+			console.log();
+		});
+	});
+	TC.catch(function (err) {
+		if (err) {
+			cb(err);
+		}
+	});
 };
-
 
 /**
  * Read the json file with the timecard data.
@@ -279,23 +237,18 @@ TimeCard.prototype.print = function(cb) {
  * @param filepath - the filepath to search for the timecard data
  * @returns {*} Promise
  */
-function getTimeCardData(filepath){
 
-    return new Promise(function(resolve, reject){
-
-        fs.readFile(filepath, 'utf8', function(err, data){
-
-            if (err){
-               reject(err)
-            } else {
-                resolve(JSON.parse(data));
-            }
-
-        });
-
-    });
+function getTimeCardData(filepath) {
+	return new Promise(function (resolve, reject) {
+		fs.readFile(filepath, 'utf8', function (err, data) {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(JSON.parse(data));
+			}
+		});
+	});
 }
-
 
 /**
  * Physically write the timecard data to the json file.
@@ -303,17 +256,15 @@ function getTimeCardData(filepath){
  * @param {object} data - the json data to write
  * @param {function} cb - the callback
  */
-TimeCard.prototype.writeTimeCard = function(data, cb) {
 
-    fs.writeFile(this.filepath, data, function (err) {
-        if (err) {
-            throw err;
-        }
-        cb();
-    });
-
+TimeCard.prototype.writeTimeCard = function (data, cb) {
+	fs.writeFile(this.filepath, data, function (err) {
+		if (err) {
+			throw err;
+		}
+		cb();
+	});
 };
-
 
 /**
  * Pretty-print a successful 'timecard new' message.
@@ -321,29 +272,29 @@ TimeCard.prototype.writeTimeCard = function(data, cb) {
  * @note: This gets called after a new timecard
  * has been created with `timecard new`
  */
-function reportSuccessfulNewTimeCard(){
-    message.print(cliTimecard.filepath);
-    console.log(messages.createdNewTimeCard);
-}
 
+function reportSuccessfulNewTimeCard() {
+	message.print(cliTimecard.filepath);
+	console.log(messages.createdNewTimeCard);
+}
 
 /**
  * Pretty-print a successful clockin message.
  *
  */
-function reportSuccessfulClockIn(){
-    console.log(messages.successfulClockin);
-}
 
+function reportSuccessfulClockIn() {
+	console.log(messages.successfulClockin);
+}
 
 /**
  * Pretty-print a successful clockout message.
  *
  */
-function reportSuccessfulClockOut(){
-    console.log(messages.successfulClockout);
-}
 
+function reportSuccessfulClockOut() {
+	console.log(messages.successfulClockout);
+}
 
 /**
  * Timecard's point of entry.
@@ -354,111 +305,84 @@ function reportSuccessfulClockOut(){
  * @param {Array} args - cli args
  * @param {Object} options - cli options
  */
-function init(args, options){
 
-    //console.log("called init with arguments: ", args);
-    //console.log("called init with options: ", options);
+function init(args, options) {
+	if (options.version || options.v) {
+		console.log(pkg.version);
+		process.exit();
+	}
 
-    if (options.version || options.v){
-        console.log(pkg.version);
-        process.exit();
-    }
+	if (options.help || options.h) {
+		showHelp();
+		process.exit();
+	}
 
-    if (options.help || options.h){
-        showHelp();
-        process.exit();
-    }
+	if (args.length === 0) {
+		showHelp();
+		process.exit(1);
+	}
 
-    if (args.length === 0){
-        showHelp();
-        process.exit(1);
-    }
+	cliTimecard = new TimeCard();
 
-    cliTimecard = new TimeCard();
-
-
-    if (args.indexOf('new') > -1){
-        cliTimecard.createBlankTimeCard();
-    }
-
-    else if (args.indexOf('clockin') > -1){
-        cliTimecard.clockin(function(err){
-            if(err){
-                console.log(err.message);
-            }
-        });
-    }
-
-    else if (args.indexOf('clockout') > -1){
-        cliTimecard.clockout(function(err){
-            if(err){
-                console.log(err.message);
-            }
-        });
-    }
-
-    else if (args.indexOf('print') > -1) {
-        cliTimecard.print(function (err) {
-            if (err) {
-                console.log(err.message);
-            }
-        });
-    }
-
+	if (args.indexOf('new') > -1) {
+		cliTimecard.createBlankTimeCard();
+	} else if (args.indexOf('clockin') > -1) {
+		cliTimecard.clockin(function (err) {
+			if (err) {
+				console.log(err.message);
+			}
+		});
+	} else if (args.indexOf('clockout') > -1) {
+		cliTimecard.clockout(function (err) {
+			if (err) {
+				console.log(err.message);
+			}
+		});
+	} else if (args.indexOf('print') > -1) {
+		cliTimecard.print(function (err) {
+			if (err) {
+				console.log(err.message);
+			}
+		});
+	}
 }
-
 
 /**
  * Pretty-print Timecard's help information with `timecard --help`
  *
  */
-function showHelp(){
-    console.log(multiline(function () {
-/*
 
+function showHelp() {
+	console.log(multiline(function () {
+		/*
+		 Record your project development time.
 
-    Record your project development time.
+		 Get timecard setup with the `new` command, then use the `clockin` and `clockout` commands
+		 to record your time. When you want to see a summary of your time, use the `print` command.
 
-    Get timecard setup with the `new` command, then use the `clockin` and `clockout` commands
-    to record your time. When you want to see a summary of your time, use the `print` command.
+		 Commands
+		 timecard new            setup a blank timecard for your project
+		 timecard clockin        set the start time
+		 timecard clockout       set the end time
+		 timecard print          print a summary of your time
 
-    Commands
-        timecard new            setup a blank timecard for your project
-        timecard clockin        set the start time
-        timecard clockout       set the end time
-        timecard print          print a summary of your time
-
-    Options
-        -h, --help              Show this help message
-        -v, --version           Show the current timecard version
-
-
-*/
-
-    }));
+		 Options
+		 -h, --help              Show this help message
+		 -v, --version           Show the current timecard version
+		 */
+	}));
 }
-
 
 /**
  * Collect the arguments and options from the command line.
  *
  */
+
 if (process.stdin.isTTY) {
-    init(args, options);
+	init(args, options);
 } else {
-    getStdin(function (data) {
-        [].push.apply(args, data.trim().split(/\r?\n/));
-        init(args, options);
-    });
+	getStdin(function (data) {
+		[].push.apply(args, data.trim().split(/\r?\n/));
+		init(args, options);
+	});
 }
-
-
-
-
-
-
-
-
-
-
-
