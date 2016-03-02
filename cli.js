ef6d6b381e1e2966ddfdedbd634a85cc78dd236b
Michael Wuergler <wuergler@gmail.com>
2016-03-02 09:46:40 +0000

    add aliases

--- cli.js
+++ cli.js
@@ -2,8 +2,8 @@
 /* eslint-disable no-inline-comments */
 'use strict';
 var updateNotifier = require('update-notifier');
-var multiline = require('multiline');
 var Timecard = require('./dist/index.js');
+var multiline = require('multiline');
 var subarg = require('subarg');
 var meow = require('meow');
 
@@ -13,37 +13,50 @@ var cli = meow(multiline(function () {/*
   to record your time. When you want to see a summary of your time, use the 'print' command.
 
   Commands
-    timecard new            setup a blank timecard for your project
-    timecard clockin        set the start time
-    timecard clockout       set the end time
-    timecard print          print a summary of your time
+    timecard new            Setup a blank timecard for your project
+    timecard clockin        Set the start time for your shift
+    timecard clockout       Set the end time for your shift
+    timecard print          Print a summary of your time
 
   Options
     -h, --help              Show this help message
     -v, --version           Show the current timecard version
+    -i, --clockin           Alias for the clockin command
+    -o, --clockout          Alias for the clockout command
+    -n, --new               Alias for the new command
+    -p, --print             Alias for the print command
 
-*/}));
+*/}), {
+	alias: {
+		h: 'help',
+		v: 'version',
+		i: 'clockin',
+		o: 'clockout',
+		p: 'print',
+		n: 'new'
+	}
+});
 
-function init(args) {
-	if (args.length === 0) {
+function init(args, options) {
+	if (args.length === 0 && Object.keys(options).length === 0) {
 		cli.showHelp(1);
 	}
 
 	var timecard = new Timecard({filepath: process.cwd()});
 
-	if (args.indexOf('new') > -1) {
+	if (args.indexOf('new') > -1 || options.new) {
 		timecard.create();
 	}
 
-	if (args.indexOf('clockin') > -1) {
+	if (args.indexOf('clockin') > -1 || options.clockin) {
 		timecard.clockin();
 	}
 
-	if (args.indexOf('clockout') > -1) {
+	if (args.indexOf('clockout') > -1 || options.clockout) {
 		timecard.clockout();
 	}
 
-	if (args.indexOf('print') > -1) {
+	if (args.indexOf('print') > -1 || options.print) {
 		timecard.printTimecard();
 	}
 }
