fde8948a06cc5435ab0190037358df6f83d32a93
Michael Wuergler <wuergler@gmail.com>
2016-07-12 15:04:35 +0100

    code cleanup; more test coverage

--- test/test.js
+++ test/test.js
@@ -27,8 +27,9 @@ test.serial('creates a new timecard', async t => {
 });
 
 test.serial('create() returns a promise', async t => {
-	const tc = await timecard.create();
-	t.is(tc.totalSeconds, 0);
+	timecard.create().then(result => {
+		t.true(result);
+	});
 });
 
 test.serial('sets project name', async t => {
@@ -54,6 +55,28 @@ test.serial('clockin', async t => {
 	t.is(typeof data.shifts[0].startTime, 'string');
 });
 
+test.serial('report successful clockin', async t => {
+	await timecard.create().then(message => {
+		t.true(message.search(/You have created a new timecard file/g) > 0);
+	});
+});
+
+test.serial('noTimeCardFoundForClockin', async t => {
+	t.throws(timecard.clockin(), /You must create a timecard before clocking in/g);
+});
+
+test.serial('noTimeCardFoundForClockout', async t => {
+	t.throws(timecard.clockout(), /You must create a timecard before clocking out/g);
+});
+
+test.serial('allow print before clockout', async t => {
+	await timecard.create();
+	await timecard.clockin();
+	await timecard.printTimecard().then(output => {
+		t.true(/\[clockout pending\]/g.test(output));
+	});
+});
+
 test.serial('clockout', async t => {
 	await timecard.create();
 	await timecard.clockin();
@@ -70,6 +93,19 @@ test.serial('clockout', async t => {
 	t.is(typeof shift.endTime, 'string');
 });
 
+test.serial('clockout pending', async t => {
+	await timecard.create();
+	await timecard.clockin();
+
+	t.throws(timecard.clockin(), /You must clockout before clocking in/g);
+});
+
+test.serial('clockin pending', async t => {
+	await timecard.create();
+
+	t.throws(timecard.clockout(), /You must clockin before clocking out/g);
+});
+
 test.serial('records total seconds', async t => {
 	await timecard.create();
 	await timecard.clockin();
@@ -78,27 +114,6 @@ test.serial('records total seconds', async t => {
 	await timecard.clockout();
 
 	t.true(timecard.totalSeconds >= 3);
-});
-
-test.serial('utils.processTimeCardData', async t => {
-	await timecard.create();
-	await timecard.clockin();
-	timecard.processTimeCardData().then(data => {
-		t.true(data);
-		t.is(typeof data, 'object');
-		t.is(data[0].id, 0);
-	});
-});
-
-test.serial('utils.blankTimecard', t => {
-	t.is(timecard.blankTimecard('test-project'), '{\n  "project": "test-project",\n  "shifts": [],\n  "totals": {\n    "hours": 0,\n    "minutes": 0,\n    "seconds": 0\n  }\n}');
-});
-
-test.serial('utils.writeTimeCard', async t => {
-	await timecard.writeTimeCard('HEY!');
-
-	let data = await pify(fs.readFile)(timecardPath, 'utf8');
-	t.is(data, 'HEY!');
 });
 
 test.serial('prints output', async t => {
@@ -126,4 +141,59 @@ test.serial('prompts: projectName.validate', t => {
 
 test.serial('prompts: eraseThenCreatePrompt.projectName.when', t => {
 	t.is(eraseThenCreatePrompt()[1].when({eraseCard: true}), true);
+});
+
+test.serial('utils.processTimeCardData', async t => {
+	await timecard.create();
+	await timecard.clockin();
+	timecard.processTimeCardData().then(data => {
+		t.true(data);
+		t.is(typeof data, 'object');
+		t.is(data[0].id, 0);
+	});
+});
+
+test.serial('utils.blankTimecard', t => {
+	t.is(timecard.blankTimecard('test-project'), '{\n  "project": "test-project",\n  "shifts": [],\n  "totals": {\n    "hours": 0,\n    "minutes": 0,\n    "seconds": 0\n  }\n}');
+});
+
+test.serial('utils.writeTimeCard', async t => {
+	await timecard.writeTimeCard('foo!');
+
+	let data = await pify(fs.readFile)(timecardPath, 'utf8');
+	t.is(data, 'foo!');
+});
+
+test.serial('utils.checkForValidTimecard => true', async t => {
+	await timecard.create();
+	const isValid = await timecard.checkForValidTimecard();
+	t.is(isValid, true);
+});
+
+test.serial('utils.checkForValidTimecard => false', async t => {
+	await timecard.create();
+	await timecard.writeTimeCard('foo!');
+	const isValid = await timecard.checkForValidTimecard();
+	t.is(isValid, false);
+});
+
+test.serial('utils.checkForValidTimecard => false (when error getting timecard data)', async t => {
+	const tc = new Timecard({filepath: 'no/file/here'});
+	const isValid = await tc.checkForValidTimecard();
+	t.is(isValid, false);
+});
+
+test.serial('utils.processTimeCardData catch', async t => {
+	const tc = new Timecard({filepath: 'no/file/here'});
+	t.throws(tc.processTimeCardData(), /ENOENT/g);
+});
+
+test.serial('utils.writeTimeCard catch', async t => {
+	const tc = new Timecard({filepath: 'no/file/here'});
+	t.throws(tc.writeTimeCard('foo'), /ENOENT/g);
+});
+
+test.serial('utils.trace', async t => {
+	const tc = new Timecard({filepath: 'no/file/here'});
+	t.throws(tc.writeTimeCard('foo'), /ENOENT/g);
 });
